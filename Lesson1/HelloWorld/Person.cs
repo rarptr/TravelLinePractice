2e2c7ec3796c63200ceef11aa133ea2b6572a2bc
rar_dev <davidtri2013@yandex.ru>
2023-03-05 17:15:23 +0300

    first commit

---
+++ Lesson1/HelloWorld/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    // internal - виден внутри сборки
+    internal class Person
+    {
+        // Внутри класса 
+        private string _name;
+
+        // свойства = методы
+        public string Name
+        {
+            get { return _name; }
+            // set - передаваемый параметр
+            set { _name = value; }
+        }
+
+        public Person(string name)
+        {
+            _name = name;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetName(string value)
+        {
+            _name = value;
+        }
+    }
+}
---
+++ Lesson1/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using HelloWorld;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Person person = new Person("Cat");
+
+        //person.SetName("Dog");
+
+        person.Name = "Dog";
+
+        Console.WriteLine(person.Name);
+    }
+}
